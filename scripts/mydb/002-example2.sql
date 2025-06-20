a030a2f1d683d8fcf2275447db55e4e908a58d52
hieudien14310 <tranhieu14310@gmail.com>
2025-06-20 22:58:31 +0700

    update

--- scripts/mydb/002-example2.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE user_meta_data (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),Add commentMore actions
-    email VARCHAR(100) UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
