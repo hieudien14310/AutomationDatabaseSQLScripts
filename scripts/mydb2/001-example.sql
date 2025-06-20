267490bfbbafc6404ec14f827905fd7ed82c5c8f
hieudien14310 <tranhieu14310@gmail.com>
2025-06-20 23:26:50 +0700

    rm

--- scripts/mydb2/001-example.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),Add commentMore actions
-    email VARCHAR(100) UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
