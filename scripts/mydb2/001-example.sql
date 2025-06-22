72112148963fea1431ea52a9ff0e6dd20ba3a1a6
hieudien14310 <tranhieu14310@gmail.com>
2025-06-22 11:57:23 +0700

    update

--- scripts/mydb2/001-example.sql
+++ scripts/mydb2/001-example.sql
@@ -1,7 +1,6 @@
 CREATE TABLE information (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
+    name VARCHAR(100),Add commentMore actions
     email VARCHAR(100) UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    created_by VARCHAR(255) NOT NULL
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
