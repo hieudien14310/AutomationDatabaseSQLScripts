4073aa66468478f884d9bbe1ed445c202222f859
hieudien14310 <tranhieu14310@gmail.com>
2025-06-22 11:32:01 +0700

    fix

--- scripts/mydb2/003-create-cards.sql
+++ scripts/mydb2/003-create-cards.sql
@@ -1,6 +1,6 @@
 CREATE TABLE cards_data (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(sjksdhkjhk),
+    name VARCHAR(100) not null,
     email VARCHAR(100) UNIQUE,
     number text not null,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
