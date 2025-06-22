063ce2cd36d806ee1a22b4dab81b4489e853c2fb
hieudien14310 <tranhieu14310@gmail.com>
2025-06-22 11:53:36 +0700

    text no_update_old_file_sql

--- scripts/mydb2/001-example.sql
+++ scripts/mydb2/001-example.sql
@@ -2,5 +2,6 @@ CREATE TABLE information (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),Add commentMore actions
     email VARCHAR(100) UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by VARCHAR(255) not null,
 );
